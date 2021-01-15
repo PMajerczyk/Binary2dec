e7d373ed69c2bca561678e78a1c33af89cbf3805
PMajerczyk <pawel.majerczyk@onet.eu>
2021-01-15 21:50:02 +0100

    Konwerter

--- binary2dec/binary2dec/main.cpp
+++ binary2dec/binary2dec/main.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
+#include <math.h>
 
 int main()
 {
 	std::cout << "Dzien dobry\n";
-	std::cout << "Wpisz liczbe binarna\n";
 	std::string liczba1;
 	std::string wybor;
 	bool dzia³a1 = false;
 	bool dzia³a = true;
-
-
-
-	for
-
-
-
+	int wynik;
+	wynik = 0;
+	int dlugosc;
+	int i;
+	i = 0;
 
 	while (dzia³a == true)
 	{
+		std::cout << "Wpisz liczbe binarna\n";
 		std::cin >> liczba1;
-		std::cout << "wprowadzona liczba wynosi:\t" + liczba1 + "\n";
+		dlugosc = liczba1.length();
+		do
+		{
+			wynik += (liczba1[dlugosc - 1] - 48) * pow(2, i);
+			i++;
+			dlugosc--;
+		} while (dlugosc > 0);
+		std::cout << "Wprowadzona liczba wynosi:\t" << wynik << "\n";
+	    i = 0;
+		wynik = 0;
+		dlugosc = 0;
 
 		do
 		{
-
 			std::cout << "kontynuowac ?\n";
 			std::cin >> wybor;
 
@@ -36,6 +44,7 @@ int main()
 			{
 				dzia³a = false;
 				dzia³a1 = true;
+				std::cout << "Dziekuje, milego dnia :)\n";
 			}
 			else
 			{
@@ -43,8 +52,7 @@ int main()
 				dzia³a1 = false;
 			}
 		} 
-		while (dzia³a1 == false);
-		
+		while (dzia³a1 == false);	
 	}
 	system("pause");
 }
